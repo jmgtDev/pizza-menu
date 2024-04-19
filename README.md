ee516e813f42db3cddcf1a00e4865a50d5bd3a83
João Marcos Tavares <jmgt.dev@gmail.com>
2024-04-19 02:25:39 -0300

    Criação do branch 4

--- README.md
+++ README.md
@@ -30,3 +30,15 @@ Cada etapa, ou cada novo branch, terá suas novidades listadas aqui, com identif
 > 3- Componentes Pizza saíram do componente App e foram para o componente Menu, para simplificação do componente App.
 
 > 4- Componente Header, Menu e Footer colocados dentro do componente App.
+
+**Branch 4:**
+
+> 1- Nesse branch foi introduzido estilo CSS para a página de algumas formas diferentes.
+
+> 2- Adição de estilo CSS inline no componente Footer. Essa estilização não é a ideal, pois deixa o código mais difícil de ler. Está nesse branch para questões de aprendizado, mas será retirada no próximo.
+
+> 3- Novo arquivo CSS (index.css) introduzido.
+
+> 4- index.css foi importado para o index.js.
+
+> 5- Adição de estilo nos componentes App, Header, Menu e Pizza através de classes que vêm do arquivo index.css.
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css"; //importação de arquivo CSS global pelo webpack.
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
       <Menu />
       <Footer />
@@ -12,13 +13,18 @@ function App() {
 }
 
 function Header() {
-  return <h1>FAST REACT PIZZA CO.</h1>;
+  return (
+    /* estilização através de classes. Observe que utiliza-se className e não class, pois class é uma palavra reservada do JS. */
+    <header className="header">
+      <h1>FAST REACT PIZZA CO.</h1>
+    </header>
+  );
 }
 
 function Menu() {
   return (
-    <div>
-      <h3>Our Menu:</h3>
+    <div className="menu">
+      <h2>Our Menu</h2>
       <Pizza />
       <Pizza />
       <Pizza />
@@ -28,9 +34,9 @@ function Menu() {
 
 function Pizza() {
   return (
-    <div>
+    <div className="pizza">
       <img src="images/spinaci.jpg" alt="spinaci pizza" />
-      <h1>Pizza Spinaci</h1>
+      <h3>Pizza Spinaci</h3>
       <p>Ingredients: Tomato, mozarella, spinach, and ricotta cheese</p>
     </div>
   );
@@ -38,9 +44,10 @@ function Pizza() {
 
 function Footer() {
   return (
-    /* new Date().toLocaleTimeString() é para colocar em string o horário local. */
-    <p>
-      Time:{new Date().toLocaleTimeString()} We are open ultil 22:00. Come visit
+    /*   estilização inline dentro do JSX. Primeiro precisa abrir chaves para pode usar JS e depois precisa de mais um par de chaves porque a estilização é um objeto. Além disso, nos nomes dos estilos não são aceitos traços, sendo necessário o uso do camelCase */
+    /* essa estilização poderia ser colocada dentro de uma variável e depois a variável poderia ser passada para dentro do jsx. */
+    <p style={{ fontSize: "12px" }}>
+      Time:{new Date().toLocaleTimeString()} We are open until 22:00. Come visit
       us or order online.
     </p>
   );
