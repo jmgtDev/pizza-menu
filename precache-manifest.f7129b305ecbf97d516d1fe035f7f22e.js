8b7acf7c193f293f56ca38769b02f18c253cf6df
João Marcos Tavares <jmgt.dev@gmail.com>
2024-04-25 03:56:26 -0300

    Updates

---
+++ precache-manifest.f7129b305ecbf97d516d1fe035f7f22e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d99d0f771b5db2bec8d68b10d3a7a9e",
+    "url": "/pizza-menu/index.html"
+  },
+  {
+    "revision": "a77d57465fcb07ce54a9",
+    "url": "/pizza-menu/static/css/main.05fbbff1.chunk.css"
+  },
+  {
+    "revision": "1f5929bab9e74fc2d234",
+    "url": "/pizza-menu/static/js/2.a71b1702.chunk.js"
+  },
+  {
+    "revision": "a77d57465fcb07ce54a9",
+    "url": "/pizza-menu/static/js/main.1f3eb768.chunk.js"
+  },
+  {
+    "revision": "a8638b09d05fd16587f5",
+    "url": "/pizza-menu/static/js/runtime~main.583274af.js"
+  }
+]);
